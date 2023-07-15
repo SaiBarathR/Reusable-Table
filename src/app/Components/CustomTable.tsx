7377c793a96b47182e32fc74461477ffb9c4ec27
Sai Barath <saibarathdeep@outlook.com>
2023-07-15 14:00:11 +0530

    added charkraui to the '/' endpoint with table component

---
+++ src/app/Components/CustomTable.tsx
@@ -0,0 +1,9 @@
+
+export default function CustomTable() {
+
+    return <div className="text-black">
+        hi
+    </div>
+
+}
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Providers } from "./Providers";
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
