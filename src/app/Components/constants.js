77c0da4824038816d7b33e913508213d9af521ba
Sai Barath <saibarathdeep@outlook.com>
2023-07-15 19:03:03 +0530

    added contants file temporarily

---
+++ src/app/Components/constants.js
@@ -0,0 +1,38 @@
+export const rowsObject = [
+    {
+        timeStamp: '35 minutes ago',
+        purchaseId: 265845121,
+        mail: 'saibarathdeep@outlook.com',
+        name: 'Sai',
+        source: '',
+        status: false,
+        select: "selected"
+    },
+    {
+        timeStamp: '35 minutes ago',
+        purchaseId: 265845121,
+        mail: 'saibarathdeep@outlook.com',
+        name: 'Sai',
+        source: '',
+        status: false,
+        select: "selected"
+    }, {
+        timeStamp: '35 minutes ago',
+        purchaseId: 265845121,
+        mail: 'saibarathdeep@outlook.com',
+        name: 'Sai',
+        source: '',
+        status: false,
+        select: "selected"
+    }
+    , {
+        timeStamp: '35 minutes ago',
+        purchaseId: 265845121,
+        mail: 'saibarathdeep@outlook.com',
+        name: 'Sai',
+        source: '',
+        status: false,
+        select: "selected"
+    }
+]
+
