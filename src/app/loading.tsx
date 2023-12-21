fda36a764a295cd8b89111cd5944a769abc3697c
Sai Barath <saibarathr@gmail.com>
2023-12-21 11:43:40 +0530

    Remove import of tableStyle.css in Loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Skeleton } from "@chakra-ui/react";
-import "./Components/table/tableStyle.css"
 
 export default function Loading() {
     return <div className=" w-full flex flex-col gap-4 text-black min-w-[500px] border custom-table-container p-5">
