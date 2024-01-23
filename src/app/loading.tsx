2a0c120ceefe36ac110ca58e419b3f495532690e
Sai Barath <saibarathr@gmail.com>
2024-01-23 15:34:14 +0530

    Update Loading component styles

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@
 import { Skeleton } from "@chakra-ui/react";
 
 export default function Loading() {
-    return <div className=" w-full flex flex-col gap-4 text-black min-w-[500px] border custom-table-container p-5">
+    return <div className=" w-11/12 flex flex-col mt-4 gap-4 text-black  border custom-table-container p-5">
         {[...Array(6).keys()].map((i: number) => <Skeleton key={i} height='50px' rounded={"lg"} />)}
     </div>
 }
